c8fdbe3afa7cf0a0d7d96718ce3265634a026ec6
Luca Massaron <lucamassaron@gmail.com>
2017-09-23 00:39:27 +0200

    added cnn for traffic light state detection from a cropped image

--- ros/src/tl_detector/light_classification/tl_classifier_cnn.py
+++ ros/src/tl_detector/light_classification/tl_classifier_cnn.py
@@ -2,6 +2,41 @@ from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import cv2
 
+class CNNTLStateDetector(object):
+    def __init__(self):
+        self.session = tf.Session()
+        loader = tf.train.import_meta_graph('state_detector.meta')
+        loader.restore(self.session, "./state_detector")
+        graph = tf.get_default_graph()
+        self.out = graph.get_tensor_by_name("output_class:0")
+        self.input_layer = graph.get_tensor_by_name("input_layer:0")
+        self.keep_prob = graph.get_tensor_by_name("keep_prob:0")
+
+    def get_classification(self, image):
+        """
+        Determines the color of the traffic light in a 
+        cropped portion of the image
+
+        Args:
+            image (cv::Mat): image containing the cropped 
+            image of a traffic light
+
+        Returns:
+            int: ID of traffic light color (as specified in styx_msgs/TrafficLight)
+
+        """
+        RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        result = self.session.run([self.out],
+                                    feed_dict={self.input_layer: [RGB_image],
+                                               self.keep_prob: 1.0})
+        if int(result[0][0]) == 0:
+            return TrafficLight.RED
+        elif int(result[0][0]) == 1:
+            return TrafficLight.YELLOW
+        elif int(result[0][0]) == 2:
+            return TrafficLight.GREEN
+        else:
+            return TrafficLight.UNKNOWN
 
 class CNNTLClassifier(object):
     def __init__(self):
@@ -15,7 +50,8 @@ class CNNTLClassifier(object):
         self.counter = 0
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        """
+        Determines the color of the traffic light in the image
 
         Args:
             image (cv::Mat): image containing the traffic light
